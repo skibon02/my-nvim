5eb6379456afd0f6a7ad3ba8d4c62479f7525e20
SkyGrel19 <skibav2002@gmail.com>
2023-04-26 01:11:55 +0300

    fix run project

--- after/plugin/floaterm.lua
+++ after/plugin/floaterm.lua
@@ -1,4 +1,4 @@
 vim.keymap.set("t", "<F8>", "<C-\\><C-n><cmd>FloatermToggle<CR>")
 vim.keymap.set("n", "<F8>", "<cmd>FloatermToggle<CR>")
 
-vim.keymap.set('n', '<leader>t', function() vim.api.nvim_command('FloatermNew ' .. vim.fn.input('CMD: ')) end, {remap = true})
+vim.keymap.set('n', '<leader>tf', function() vim.api.nvim_command('FloatermNew ' .. vim.fn.input('CMD: ')) end, {remap = true})
