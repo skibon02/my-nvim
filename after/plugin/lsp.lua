c3d81790b5d13049073b36662f7c9002fcc7971e
SkyGrel19 <skibav2002@gmail.com>
2023-04-26 17:30:03 +0300

    Autocompletion fixes, remap fixes, rust-analyzer config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,54 +1,58 @@
 
 
--- Completion Plugin Setup
+--- Completion Plugin Setup
+
+-- Set completeopt to have a better completion experience
+-- :help completeopt
+-- menuone: popup even when there's only one match
+-- noinsert: Do not insert text until a selection is made
+-- noselect: Do not auto-select, nvim-cmp plugin will handle this for us.
+vim.o.completeopt = "menuone,noinsert,noselect"
+
+-- Avoid showing extra messages when using completion
+vim.opt.shortmess = vim.opt.shortmess + "c"
 local cmp = require'cmp'
 cmp.setup({
-  -- Enable LSP snippets
-  snippet = {
-    expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body)
-    end,
-  },
-  mapping = {
-    -- Add tab support
-    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-    ['<Tab>'] = cmp.mapping.select_next_item(),
+    -- Enable LSP snippets
+    preselect = cmp.PreselectMode.None,
+    snippet = {
+        expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+        end,
+    },
+    mapping = {
+        -- Add tab support
+        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+        ['<Tab>'] = cmp.mapping.select_next_item(),
 
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
 
-    --['<C-q>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({ -- Enter to complete
-      behavior = cmp.ConfirmBehavior.Insert,
-      select = true,
+
+        ['<CR>'] = cmp.mapping.confirm({ -- Enter to complete
+        behavior = cmp.ConfirmBehavior.Insert,
+        select = true,
     })
-  },
-  -- Installed sources:
-  sources = {
+},
+-- Installed sources:
+sources = {
     { name = 'path' },                              -- file paths
-    { name = 'nvim_lsp', keyword_length = 2 },      -- from language server
+    { name = 'nvim_lsp'},      -- from language server
     { name = 'nvim_lsp_signature_help'},            -- display function signatures with current parameter emphasized
     { name = 'nvim_lua', keyword_length = 2},       -- complete neovim's Lua runtime API such vim.lsp.*
-    { name = 'buffer', keyword_length = 2 },        -- source current buffer
-    { name = 'vsnip', keyword_length = 2 },         -- nvim-cmp source for vim-vsnip 
-    { name = 'calc'},                               -- source for math calculation
-  },
-  window = {
-      --completion = cmp.config.window.bordered(),
-      --documentation = cmp.config.window.bordered(),
-  },
-  formatting = {
-      fields = {'menu', 'abbr', 'kind'},
-  },
+    --{ name = 'buffer', keyword_length = 2 },        -- source current buffer
+    --{ name = 'vsnip', keyword_length = 2 },         -- nvim-cmp source for vim-vsnip 
+    --{ name = 'calc'},                               -- source for math calculation
+},
+window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+},
+formatting = {
+    fields = {'menu', 'abbr', 'kind'},
+},
 })
 
-
-vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end)
-vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end)
-vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end)
-vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end)
-
-vim.keymap.set("i", "<c-h>", function() vim.lsp.buf.signature_help() end)
 
 -- Lua --
 
--- after/plugin/rust-tools.lua
+++ after/plugin/rust-tools.lua
@@ -8,19 +8,47 @@ local codelldb_path = codelldb_dir .. "extension/adapter/codelldb"
 local liblldb_path = codelldb_dir .. "extension/lldb/lib/liblldb.so"
 
 rt.setup({
+    tools = {
+        runnables = {
+            use_telescope = true,
+        },
+        inlay_hints = {
+            auto = true,
+            show_parameter_hints = false,
+            parameter_hints_prefix = "",
+            other_hints_prefix = "",
+        },
+    },
     server = {
         on_attach = function(_, bufnr)
             local keymap_opts = { buffer = bufnr }
             -- Hover actions
             vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, keymap_opts )
             -- code action groups
-            vim.keymap.set("n", "<Leader>.", rt.code_action_group.code_action_group, keymap_opts)
+            --vim.keymap.set("n", "<Leader>.", rt.code_action_group.code_action_group, keymap_opts)
 
             -- Runnables
             vim.keymap.set("n", "<leader>rr", rt.runnables.runnables)
             vim.keymap.set("n", "<leader>rd", rt.debuggables.debuggables)
 
+            -- Floaterm
             vim.keymap.set('n', '<leader>tr', "<cmd>FloatermNew cargo run<cr>")
+
+            -- LSP
+            vim.keymap.set("n", "H", vim.lsp.buf.hover, keymap_opts)
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, keymap_opts)
+            vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, keymap_opts)
+            vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, keymap_opts)
+            vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, keymap_opts)
+
+            vim.keymap.set("i", "<c-h>", vim.lsp.buf.signature_help, keymap_opts)
+
+            vim.keymap.set("n", "ga", vim.lsp.buf.code_action, keymap_opts)
+
+            -- Goto previous/next diagnostic warning/error
+            vim.keymap.set("n", "g[", vim.diagnostic.goto_prev, keymap_opts)
+            vim.keymap.set("n", "g]", vim.diagnostic.goto_next, keymap_opts)
+
         end,
         settings = {
             -- to enable rust-analyzer settings visit:
@@ -30,6 +58,11 @@ rt.setup({
                 checkOnSave = {
                     command = "clippy",
                 },
+                completion = {
+                    callable = {
+                        snippets = "add_parentheses"
+                    }
+                }
             },
         },
     },
