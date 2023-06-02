7352709528045592fe98e00642cd392dfb59883e
SkyGrel19 <skibav2002@gmail.com>
2023-06-02 22:10:08 +0300

    another one

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,32 +4,14 @@ return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
-    use {
-        'nvim-telescope/telescope.nvim', branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
-
-    -- themes
-    use { "catppuccin/nvim", as = "catppuccin" }
     use { "ellisonleao/gruvbox.nvim" }
-    use { "shaunsingh/nord.nvim" }
-    use { "Mofiqul/vscode.nvim" }
-    use { "chriskempson/base16-vim" }
-    use { "jacoborus/tender.vim" }
-    use 'Mofiqul/dracula.nvim'
 
     use { 'williamboman/mason.nvim',
         config = function()
             require("mason").setup({})
         end }
-    use { 'williamboman/mason-lspconfig.nvim',
-        config = function()
-            require("mason-lspconfig").setup({
-            })
-        end }
 
     use 'neovim/nvim-lspconfig'
-    use 'simrat39/rust-tools.nvim'
 
     -- Completion framework:
     use 'hrsh7th/nvim-cmp'
@@ -56,11 +38,6 @@ return require('packer').startup(function(use)
             require('Comment').setup()
         end
     }
-
-    --navigation
-    --use 'theprimeagen/harpoon'
-    use 'theprimeagen/vim-be-good'
-
     -- debugging
     use { 'mfussenegger/nvim-dap' }
     use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } }
