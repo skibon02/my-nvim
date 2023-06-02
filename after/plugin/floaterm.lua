470bd8ca27790b5abd8e507fd10dcbd1ce42d203
SkyGrel19 <skibav2002@gmail.com>
2023-06-02 22:01:28 +0300

    remove configs

--- after/plugin/floaterm.lua
+++
@@ -1,5 +0,0 @@
-vim.keymap.set("t", "<c-r>", "<C-\\><C-n><cmd>FloatermToggle<CR>", {noremap = true, silent = true, desc = "Toggle terminal"})
-vim.keymap.set("t", "<c-s>", "<C-\\><C-n><cmd>FloatermKill<CR>", {noremap = true, silent = true, desc = "Close terminal"})
-vim.keymap.set("n", "<leader>tf", "<cmd>FloatermToggle<CR>", {desc = "Open terminal"})
-
-vim.keymap.set('n', '<leader>tr', function() vim.api.nvim_command('FloatermNew ' .. vim.fn.input('CMD: ')) end, {desc = "Execute single command"})
--- after/plugin/telescope.lua
+++
@@ -1,4 +0,0 @@
-local builtin = require('telescope.builtin')
-
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope' })
-vim.keymap.set('n', '<leader>ft', builtin.git_files, { desc = 'Telescope git' })
