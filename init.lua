6f60b1a8e34b43899b3fbc1766dbd86acc0fed1a
SkyGrel19 <skibav2002@gmail.com>
2023-06-03 13:33:20 +0300

    comment plugins

--- init.lua
+++ init.lua
@@ -1,21 +1,17 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-    -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
-    -- use 'neovim/nvim-lspconfig'
 
-    -- Completion framework:
+    -- Completion framework and completion source (for lua)
     use 'hrsh7th/nvim-cmp'
-
-    -- Useful completion sources:
     use 'hrsh7th/cmp-nvim-lua'
-    -- use 'hrsh7th/cmp-buffer'
 
-    -- debugging
+    -- dap and dapui
     use { 'mfussenegger/nvim-dap' }
     use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } }
 
+    -- bufferline
     use { 'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons' }
 end)
